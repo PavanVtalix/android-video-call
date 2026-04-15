b61285a95599cba5473d942bf99321884a80f246
PavanVtalix <chauhan.pavan@vtalix.in>
2026-04-15 15:42:23 +0530

    video fixes-2

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // plugins: [react()],
-    server: {
+  plugins: [react()],
+  server: {
     host: "0.0.0.0",
     port: 5175,
+    allowedHosts: [
+      "android-video-call-1-1.onrender.com"
+    ],
   },
 });
